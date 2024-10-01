c395f3fc7ccade11af150bfcfde74b8d2deb4c6e
tibthink <bent23jam@gmail.com>
2024-09-30 21:01:19 -0400

    honestly just messing around with it

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 python3 reliqua.py -m "We're no strangers to love
 You know the rules and so do I (Do I)
 A full commitment's what I'm thinking of
-You wouldn't get this from any other guy" -p 8080 -k 8 -c weed
+You wouldn't get this from any other guy" -p 8080 -k 8 -c weed -L
