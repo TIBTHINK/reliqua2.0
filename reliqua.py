d019fe48f578bfa4ba654542e2694834cf7553b8
tibthink <bent23jam@gmail.com>
2025-02-23 17:06:57 -0500

    finally fixed the zip feature (2025-02-23) [branch: main, author: tibthink]

--- reliqua.py
+++ reliqua.py
@@ -10,6 +10,7 @@ import shutil
 import platform
 import hashlib
 import sys
+import zipfile
 
 ip = rs.get_ip()
 pwd = os.getcwd()
@@ -97,6 +98,14 @@ def hashed(password):
     hash_obj = hashlib.sha256(password.encode())
     hashed_password = hash_obj.hexdigest()
     return hashed_password
+
+def zip_folder(folder_path, output_filename):
+    with zipfile.ZipFile(output_filename, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
 
 @click.command()
 @click.option("-m", "--message", help="Sets your message")
@@ -212,18 +221,22 @@ def main(message, port, keygen, server, clean, version, code, local, zip):
         path = os.path.join(pwd, "client")
         os.mkdir(path)
 
+
+
     shutil.copy2('reliqua_client.py', pwd + '/client', follow_symlinks=True)
     shutil.copy2('config.json', pwd + '/client', follow_symlinks=True)
     shutil.copy2("INSTRUCTIONS.html", pwd + '/client', follow_symlinks=True)
     shutil.copy2("setup.sh", pwd + '/client' ,follow_symlinks=True)
     shutil.copy2("setup.sh", pwd + '/client' ,follow_symlinks=True)
+
     
     if zip:
         print("zipping folder")
-        shutil.make_archive("client/", 'zip', "client")
+        zip_folder("./client", "client.zip")
         print("folder zipped and ready to ship")
+        shutil.copy2("client.zip", pwd + '/client' , follow_symlinks=True)
     else:
-        print("Send the client folder in your directory to the target")    
+        print("Send the client folder in your directory to the target") 
     
     print("Remember to portforward port " + str(port)+ " on " + rs.get_ip(True))
 
