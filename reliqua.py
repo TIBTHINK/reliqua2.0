25c48269085e83638fae492ba3477cee6ea4872f
tibthink <bent23jam@gmail.com>
2025-06-06 16:00:40 -0400

    idk how long this has been there but fixed the zip file to zip the bat script as well

--- reliqua.py
+++ reliqua.py
@@ -220,7 +220,7 @@ def main(message, port, keygen, server, clean, version, code, hint, local, zip):
     shutil.copy2('config.json', pwd + '/client', follow_symlinks=True)
     shutil.copy2("INSTRUCTIONS.html", pwd + '/client', follow_symlinks=True)
     shutil.copy2("setup.sh", pwd + '/client' ,follow_symlinks=True)
-    shutil.copy2("setup.sh", pwd + '/client' ,follow_symlinks=True)
+    shutil.copy2("setup.bat", pwd + '/client' ,follow_symlinks=True)
 
     
     if zip:
