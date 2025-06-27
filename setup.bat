038419f84b7cdf835a4fa11e1a08c7c00e4b4a3e
tibthink <bent23jam@gmail.com>
2025-06-26 23:36:42 -0400

    testing some shit out part 2

--- setup.bat
+++ setup.bat
@@ -16,15 +16,10 @@ where python >nul 2>&1
 if %errorlevel% NEQ 0 (
     echo Python not found. Downloading and installing Python...
 
-    set PYTHON_INSTALLER=%TEMP%\python-installer.exe
-    set PYTHON_URL=https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe
-
-    powershell -Command "Invoke-WebRequest -Uri '%PYTHON_URL%' -OutFile '%PYTHON_INSTALLER%'"
-
+    python3
     echo Running Python installer...
-    "%PYTHON_INSTALLER%" /quiet InstallAllUsers=1 PrependPath=1 Include_pip=1
 
-    del /f /q "%PYTHON_INSTALLER%"
+
 ) else (
     echo Python is already installed.
 )
